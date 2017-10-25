50d0e28a1573141087bd794f8c06bea6dd0b31a2
Andre Ricardo Cintra <andre.cintra@jsl.com.br>
2017-10-25 14:47:14 -0200

    Optimizing parameters (fixed in 10 digits code)

--- app/Http/Controllers/Coupons.php
+++ app/Http/Controllers/Coupons.php
@@ -34,24 +34,31 @@ class Coupons extends Controller
   public function generate($prefix, $quantity){
     $result = new \App\Coupon();
     $i = 0;
+    $maxCombs = pow(10, 10 - strlen($prefix));
 
     if($quantity > 100000):
       $result->addError("Quantidade de cupons solicitados maior que o máximo permitido");
-    elseif (strlen($prefix) > 3):
-      $result->addError("Prefixo maior que 3 caracteres");
+    elseif (strlen($prefix) < 3):
+      $result->addError("Prefixo menor que 3 caracteres");
     elseif (preg_match("/[^A-Z]/", $prefix)):
       $result->addError("Prefixo deve conter apenas caracteres maiúsculos");
+    elseif ($quantity > $maxCombs):
+      $result->addError(sprintf("Esse prefixo permite a geração de até %d códigos. %d foram solicitados", $maxCombs, $quantity));
     else:
       $code = 0;
+      $fixed = strlen($prefix);
       while($i < $quantity):
+
         $seed = rand(2,10);
         $pow = rand(7,9);
-        $code = ($code + pow($seed, $pow)) % pow(10, 7);
-        $newOne = sprintf("%s%07d", $prefix, $code);
+        $code = ($code + pow($seed, $pow)) % pow(10, 10 - $fixed);
+
+        $newOne = sprintf("%s%0". (10 - $fixed) . "d", $prefix, $code);
         if( !$result->inList($newOne) ):
           $result->addToList($newOne);
           $i++;
         endif;
+
       endwhile;
       $result->sort();
     endif;
