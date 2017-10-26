619a689472b76443fd45cc0d9d528857086455e8
Andre Ricardo Cintra <arcin_es@hotmail.com>
2017-10-26 09:11:43 -0200

    Commenting main functionalities

--- app/Coupon.php
+++ app/Coupon.php
@@ -3,41 +3,48 @@ namespace App;
 
 class Coupon {
 
-  private $list;
-  private $error;
+  private $list; // array to store all generated coupon codes
+  private $error; // array to store all error messages when validating the generation request
 
+  // Initialize object attributes, i. e., create both arrays, list and errors
   public function __construct(){
     $this->list = array();
     $this->error = array();
   }
 
+  // Insert $elem (coupon code) in object list array;
   public function addToList($elem){
     $this->list[] = $elem;
     return true;
   }
 
+  // Insert $err (error message) in object error array
   public function addError($err){
     $this->error[] = $err;
     return true;
   }
 
+  // Return object generated coupons code array (AKA list)
   public function getList(){
     return $this->list;
   }
 
+  // Return object error array
   public function getErrors(){
     return $this->error;
   }
 
+  // Sort current object list
   public function sort(){
     sort($this->list);
   }
 
+  // Verify if given $elem is in the list
   public function inList($elem){
     return in_array($elem, $this->list);
   }
 
-  // Function needed to return a JSON
+  // Convert a Coupon object to an array that will be converted in JSON in the response
   public function toArray(){
     return array( "cupons" =>
       array(
--- app/Http/Controllers/Coupons.php
+++ app/Http/Controllers/Coupons.php
@@ -4,30 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+// Controller for routes manipulation
 class Coupons extends Controller
 {
-    //
   public function generateDefault(){
     $randomPrefix = chr( rand(65, 90) ) . chr( rand(65,90) )  . chr( rand(65,90) );
     $defaultQuantity = 50000;
-
     return response()->json($this->generate($randomPrefix, $defaultQuantity));
   }
 
   public function generateWithPrefix($prefix){
     $defaultQuantity = 50000;
-
     return response()->json($this->generate($prefix, $defaultQuantity));
   }
 
   public function generateWithQuantity($quantity){
     $randomPrefix = chr( rand(65, 90) ) . chr( rand(65,90) )  . chr( rand(65,90) );
-
     return response()->json($this->generate($randomPrefix, $quantity));
   }
 
   public function generateWithBoth($prefix, $quantity){
-    // return response()->json($this->generate($prefix, $quantity));
     return response()->json($this->generate($prefix, $quantity));
   }
 
@@ -38,12 +34,16 @@ class Coupons extends Controller
 
     if($quantity > 100000):
       $result->addError("Quantidade de cupons solicitados maior que o máximo permitido");
+
     elseif (strlen($prefix) < 3):
       $result->addError("Prefixo menor que 3 caracteres");
+
     elseif (preg_match("/[^A-Z]/", $prefix)):
       $result->addError("Prefixo deve conter apenas caracteres maiúsculos");
+
     elseif ($quantity > $maxCombs):
       $result->addError(sprintf("Esse prefixo permite a geração de até %d códigos. %d foram solicitados", $maxCombs, $quantity));
+
     else:
       $code = 0;
       $fixed = strlen($prefix);
